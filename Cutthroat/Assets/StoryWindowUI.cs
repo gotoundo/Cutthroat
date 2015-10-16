8927b9ee8f99e7ba3bd0f546839ecbaec02fe9a6
gotoundo <trogdor+git@gmail.com>
2015-10-16 01:33:42 -0700

    Story Dialog Panel mocked up and hidden

---
+++ Cutthroat/Assets/StoryWindowUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StoryWindowUI : MonoBehaviour {
+    public Image Portrait;
+    public Text Dialog;
+    public Text Name;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
